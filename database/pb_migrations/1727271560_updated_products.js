ed29db618f3619316ba8fdffc4976083120d4f6c
FrShao <fredcshao@gmail.com>
2024-09-25 16:02:22 +0200

    Feat: DB update

---
+++ database/pb_migrations/1727271560_updated_products.js
@@ -0,0 +1,52 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("v7cyb4160gbf15y")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "ql9pd2vv",
+    "name": "thumbnail",
+    "type": "file",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "mimeTypes": [
+        "image/png"
+      ],
+      "thumbs": [
+        "400x400"
+      ],
+      "maxSelect": 1,
+      "maxSize": 5242880,
+      "protected": false
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("v7cyb4160gbf15y")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "ql9pd2vv",
+    "name": "thumbnail",
+    "type": "file",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "mimeTypes": [],
+      "thumbs": [],
+      "maxSelect": 1,
+      "maxSize": 5242880,
+      "protected": false
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})
---
+++ database/pb_migrations/1727272482_updated_products.js
@@ -0,0 +1,56 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("v7cyb4160gbf15y")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "ql9pd2vv",
+    "name": "thumbnail",
+    "type": "file",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "mimeTypes": [
+        "image/png"
+      ],
+      "thumbs": [
+        "600x600"
+      ],
+      "maxSelect": 1,
+      "maxSize": 5242880,
+      "protected": false
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("v7cyb4160gbf15y")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "ql9pd2vv",
+    "name": "thumbnail",
+    "type": "file",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "mimeTypes": [
+        "image/png"
+      ],
+      "thumbs": [
+        "400x400"
+      ],
+      "maxSelect": 1,
+      "maxSize": 5242880,
+      "protected": false
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})
