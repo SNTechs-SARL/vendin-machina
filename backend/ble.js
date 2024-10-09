ccf6f73db797d01c6a347c5a17f7cbc17bef26e7
FrShao <fredcshao@gmail.com>
2024-10-09 14:44:24 +0200

    Feat: logs

--- backend/ble.js
+++ backend/ble.js
@@ -5,11 +5,15 @@ const {bluetooth, destroy} = createBluetooth()
 
 const tmp = async() => {
     const adapter = await bluetooth.defaultAdapter()
-    if (! await adapter.isDiscovering())
-    await adapter.startDiscovery()
+    if (! await adapter.isDiscovering()){
+        console.log("not discovering")
+        await adapter.startDiscovery()
+    }
+        console.log("waiting")
 
     const device = await adapter.waitDevice('a8:61:0a:af:03:ba')
     await device.connect()
+        console.log("connected")
     const gattServer = await device.gatt()
     const service1 = await gattServer.getPrimaryService('uuid')
     const characteristic1 = await service1.getCharacteristic('uuid')
