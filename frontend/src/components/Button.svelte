559f0bc0fe8cf15d95d366be8921987339e1fe15
arsenedo <arsenii.nedorezov@gmail.com>
2024-10-09 13:30:33 +0200

    create buttons

--- frontend/src/components/Button.svelte
+++ frontend/src/components/Button.svelte
@@ -1,3 +1,7 @@
-<div class="bg-neutral-400">
-    1
-</div>
+<script lang="ts">
+    export let value;
+</script>
+
+<button class={`bg-neutral-400 ${value === 0 ? 'col-start-2' : ''} flex justify-center items-center`}>
+    {value}
+</button>
--- frontend/src/components/VendingMachine.svelte
+++ frontend/src/components/VendingMachine.svelte
@@ -1,13 +1,14 @@
 <script>
 	import Button from "./Button.svelte";
-
 </script>
 <div class="h-full w-full relative overflow-hidden">
     <enhanced:img src='$lib/VendingMachine.svg?enhanced' alt="da vending machine" class="w-[1250px] absolute -top-[120px] left-1/2 transform -translate-x-1/2"/>
-    <div class="absolute w-[276px] h-[320p] top-[257px] left-[1267px] box-border p-4">
-        <div class="h-full">
-
+    <div class="absolute w-[276px] h-[320px] top-[257px] left-[1267px] box-border p-4">
+        <div class="h-full w-full grid grid-cols-3 gap-5">
+            {#each [...Array(10).keys()].map(k => k == 9 ? 0 : k + 1) as value }
+                <Button value={value}/>
+            {/each}
         </div>
-        <Button/>
+
     </div>
 </div>
