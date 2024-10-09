3e5f79a64f11027fbda9a84c05f429ab05d9b011
arsenedo <arsenii.nedorezov@gmail.com>
2024-10-09 14:00:24 +0200

    display numbers

--- frontend/src/components/Button.svelte
+++ frontend/src/components/Button.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
+	import { createEventDispatcher } from "svelte";
+
     export let value;
+
+    let dispatch = createEventDispatcher();
+
+    const handleClick = () => {
+        dispatch("click", value);
+    }
 </script>
 
-<button class={`bg-neutral-400 ${value === 0 ? 'col-start-2' : ''} flex justify-center items-center`}>
+<button class={`bg-neutral-400 ${value === 0 ? 'col-start-2' : ''} flex justify-center items-center`} on:click={() => handleClick()}>
     {value}
 </button>
--- frontend/src/components/VendingMachine.svelte
+++ frontend/src/components/VendingMachine.svelte
@@ -1,14 +1,43 @@
+<style>
+    @import url('https://fonts.cdnfonts.com/css/digital-numbers');
+
+    .screen-font {
+        font-family: 'Digital Numbers', sans-serif;                                       
+    }
+</style>
+
 <script>
 	import Button from "./Button.svelte";
+
+    let command = '';
+
+    const handleCommand = (e) => {
+        if (e.detail.toString() === "C") {
+            command = "";
+            return;
+        }
+        if (command.length >= 3) return;
+        command += e.detail.toString();
+    }
+
+    const numpadGenerator = () => {
+        let arr = [...Array(10).keys()]
+        arr.splice(0, 1)
+        arr.push("C", 0)
+        return arr
+    }
 </script>
+
 <div class="h-full w-full relative overflow-hidden">
     <enhanced:img src='$lib/VendingMachine.svg?enhanced' alt="da vending machine" class="w-[1250px] absolute -top-[120px] left-1/2 transform -translate-x-1/2"/>
-    <div class="absolute w-[276px] h-[320px] top-[257px] left-[1267px] box-border p-4">
+    <div class="screen-font absolute w-[276px] h-[70px] bg-lime-200 top-[180px] left-[1267px] box-border p-6 text-4xl flex justify-end items-center border-2 border-neutral-500">
+        {command}
+    </div>
+    <div class="absolute w-[276px] h-[320px] top-[257px] left-[1267px] box-border p-4 border-2 border-neutral-600">
         <div class="h-full w-full grid grid-cols-3 gap-5">
-            {#each [...Array(10).keys()].map(k => k == 9 ? 0 : k + 1) as value }
-                <Button value={value}/>
+            {#each numpadGenerator() as value }
+                <Button value={value} on:click={handleCommand}/>
             {/each}
         </div>
-
     </div>
 </div>
