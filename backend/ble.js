414fc490e71852dbc6c141066c37f1e8d79c0949
FrShao <fredcshao@gmail.com>
2024-10-30 14:00:04 +0100

    topeo

--- backend/ble.js
+++ backend/ble.js
@@ -41,6 +41,8 @@ const tmp = async() => {
         const gattServer = await device.gatt()
         const service1 = await gattServer.services
         console.log(JSON.stringify(await gattServer.services()))
+        const characteristic1 = await service1.getCharacteristic('0000ffe0-0000-1000-8000-00805f9b34fb')
+        await characteristic1.writeValue(Buffer.from("10"))
         /* 
         const characteristic1 = await service1.getCharacteristic('uuid')
         await characteristic1.writeValue(Buffer.from("Hello world"))
