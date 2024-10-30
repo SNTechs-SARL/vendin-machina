416dafb9f1171e9f19819c6ab9b444075257824c
FrShao <fredcshao@gmail.com>
2024-10-30 14:50:40 +0100

    topo

--- backend/ble.js
+++ backend/ble.js
@@ -43,11 +43,6 @@ const tmp = async() => {
         console.log(JSON.stringify(await service1.characteristics()))
         const characteristic1 = await service1.getCharacteristic('0000ffe1-0000-1000-8000-00805f9b34fb')
         await characteristic1.writeValue(Buffer.from("10"))
-        while(true) {
-            delay(50000)
-            console.log(await characteristic1.readValue())
-            await characteristic1.writeValue(Buffer.from("10"))
-        }
         /* 
         const characteristic1 = await service1.getCharacteristic('uuid')
         await characteristic1.writeValue(Buffer.from("Hello world"))
